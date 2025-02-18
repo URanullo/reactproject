dcf7d21cd67b64ac913e2aeb4149966060298ad6
URanullo <uranullo@ssct.edu.ph>
2025-02-18 18:02:49 +0800

    first commit

---
+++ src/components/Counter.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  function incrementNumber() {
+    setCount(count + 1);
+  }
+
+  function decrementNumber() { 
+    if(count > 0){
+      setCount(count - 1);
+    }else{
+      alert("Less Than Zero");
+    }
+  }
+
+  function resetNumber() {
+    setCount(0);
+  }
+
+  return (
+    <div className="box-container counter-box">
+      <h1 className="counter-title">CUONT</h1>
+      <div className="counter-display">{count}</div>
+      <div className="counter-buttons">
+        <button onClick={incrementNumber}>Increment</button>
+        <button onClick={decrementNumber}>Decrement</button>
+        <button onClick={resetNumber} className="reset-button">Reset</button>
+      </div>
+    </div>
+  );
+}
+
+export default Counter;
